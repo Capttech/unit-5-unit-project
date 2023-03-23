bb4c068ae47b13ddfbc4d7d7da705ae70bb317d2
Bryan McMurry <bryanmcmurry7@gmail.com>
2023-03-22 22:00:22 -0500

    trying to pull

--- unitProject/config/urls.py
+++ unitProject/config/urls.py
@@ -4,14 +4,9 @@ from app.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-<<<<<<< HEAD
     path("", home, name="home"),
     path("register/", RegisterPage, name="register"),
     path("login/", LoginPage, name="login"),
     path("logout/", LogoutUser, name="logout"),
     path("profile/", AccountSettings, name="profile"),
-=======
-    path("", root, name="root"),
-    path("contact/", contact_view, name="contact"),
->>>>>>> parent of 9e76723 (I have 2 working login/ register views each)
 ]
