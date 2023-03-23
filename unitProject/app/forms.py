35c45ad464951a79c646e4188a9feb1a8784aee2
Bryan McMurry <bryanmcmurry7@gmail.com>
2023-03-22 21:52:14 -0500

    Revert "I have 2 working login/ register views each"
    This reverts commit 9e76723b9fbb354a664c5442a9bb97e431554e3e.

--- unitProject/app/forms.py
+++ unitProject/app/forms.py
@@ -1,7 +1,5 @@
-from django.forms import ModelForm
-from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from django.contrib.auth.models import User
+from django.forms import ModelForm
 from .models import *
 
 
