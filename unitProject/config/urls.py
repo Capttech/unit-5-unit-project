18cb65ed7648ef55bfa24cbf84e213bc322621f0
FacelessRobotMan <121995215+FacelessRobotMan@users.noreply.github.com>
2023-03-24 17:00:05 -0500

    Fixed

--- unitProject/config/urls.py
+++ unitProject/config/urls.py
@@ -9,4 +9,11 @@ urlpatterns = [
     path("login/", LoginPage, name="login"),
     path("logout/", LogoutUser, name="logout"),
     path("profile/", AccountSettings, name="profile"),
+    # path("", homeView, name="home"),
+    # path("templates/", templatesView, name="templates"),
+    # path("businesses/", businessesView, name="businesses"),
+    # path("profile/", profileView, name="profile"),
+    # path("signup/", signupView, name="signup"),
+    # path("login/", loginView, name="login"),
+    # path("logout/", logoutView, name="logout"),
 ]
