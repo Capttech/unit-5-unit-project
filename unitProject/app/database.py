07657554ceeb6fda57d07c902f6ce532a2939250
Capttech <31114186+Capttech@users.noreply.github.com>
2023-03-23 19:52:54 -0500

    Updated Database

--- unitProject/app/database.py
+++ unitProject/app/database.py
@@ -2,32 +2,43 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 # ==========| MANAGER INFORMATION |==========#
-class managerDatabase:
+class profileDatabase:
     userId = models.TextField()
 
 
 # ==========| BUSINESS INFORMATION |==========#
 class businessTemplateDatabase(models.Model):
+    templateId = models.TextField()
     name = models.TextField(max_length=255)
     description = models.TextField(max_length=255)
     type = models.TextField(max_length=255)
     missionStatement = models.TextField(max_length=1500)
     creationDate = models.DateField()
-    manager = models.ManyToManyField(managerDatabase, related_name="business")
+    profile = models.ForeignKey(
+        profileDatabase, related_name="business", on_delete=models.CASCADE
+    )
 
 
-class businessContactInfoDatabase(models.Model):
+class businessContactDatabase(models.Model):
     email = models.TextField(max_length=255)
     phoneNumber = models.IntegerField()
     address = models.TextField()
     business = models.ForeignKey(
-        businessDatabase, related_name="contact", on_delete=models.CASCADE
+        businessTemplateDatabase, related_name="contact", on_delete=models.CASCADE
+    )
+
+
+class businessImagesDatabase(models.Model):
+    image = models.ImageField()
+    business = models.ForeignKey(
+        businessTemplateDatabase, related_name="contact", on_delete=models.CASCADE
     )
 
 
 # ==========| GENERATED WEBSITES |==========#
 class generatedWebsites(models.Model):
     id = models.TextField()
-    business = models.ForeignKey(
-        businessDatabase, related_name="websites", on_delete=models.CASCADE
+    templateId = models.TextField()
+    profile = models.ForeignKey(
+        profileDatabase, related_name="websites", on_delete=models.CASCADE
     )
